9b229ae906fe0ddf2b4a0042e759108122fb5ab8
Neezar <rforlyffe@gmaicl.com>
2021-01-09 19:16:19 +0800

    add auth controller router and fix orm

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,18 @@ import "dotenv/config";
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import router from "./router/index";
+import { createConnection } from "typeorm";
 
 async function createServer() {
   try {
     const app = express();
-
+    await createConnection();
     app.use(cors());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(cookieParser());
 
     app.use(router);
 
--- src/router/auth.router.ts
+++ src/router/auth.router.ts
@@ -1,10 +1,24 @@
 import { Router } from "express";
-import { loginUser } from "../controller/auth.controller";
+import {
+  loginUser,
+  currentUser,
+  refreshToken,
+  registerUser,
+  revokeRefreshTokensForUser,
+  logout,
+} from "../controller/auth.controller";
+import { isAuth } from "../middlewares/isAuth";
 import { validate } from "../validators";
 import { loginRule } from "../validators/auth/login.validate";
+import { registerRule } from "../validators/auth/register.validate";
 
-const authRouter = Router();
+const router = Router();
 
-authRouter.post("/login", loginRule, validate, loginUser);
+router.post("/login", loginRule, validate, loginUser);
+router.post("/register", registerRule, validate, registerUser);
+router.post("/logout", isAuth, logout);
+router.post("/refresh_token", refreshToken);
+router.post("/revoke_access", isAuth, revokeRefreshTokensForUser);
+router.post("/me", isAuth, currentUser);
 
-export default authRouter;
+export { router as authRouter };
